43b63a643455da2e947694af16514f4a0517723e
Bathobile <pearlbathobile@gmail.com>
2023-12-12 08:24:23 -0800

    update

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,8 @@
--- lista all records in a table except those wil no name value
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+-- Lists the number of records with the same score
+-- in the table `second_table` of the database `hbtn_0c_0`
+-- in MySQL Server.
+
+SELECT score, COUNT('score') as number
+FROM second_table
+GROUP BY score
+ORDER BY score DESC;
